7f0fc096593e5b265dabb9c190ce9dcc578d99eb
Faek Soussi <soussif@gmail.com>
2020-10-24 21:58:41 +0200

    napalm_more_stuff

---
+++ napalm_files/napalm_get_bgp_nei.py
@@ -0,0 +1,17 @@
+import json
+from napalm import get_network_driver
+
+bgplist = [
+    '192.168.108.126', 
+    '192.168.108.127'
+           ]
+
+for ip_address in bgplist:
+    print ("Connecting to " + str(ip_address))
+    driver = get_network_driver('eos')
+    iosv_router = driver(ip_address, 'faek', 'faek')
+    iosv_router.open()
+    bgp_neighbors = iosv_router.get_bgp_neighbors()
+    print (json.dumps(bgp_neighbors, indent=4))
+    iosv_router.close()
+
--- napalm_files/simple_napalm_script.py
+++ napalm_files/simple_napalm_script.py
@@ -1,8 +1,32 @@
+import json
 from napalm import get_network_driver
 
-driver = get_network_driver('ios')
-iosvl2 = driver('192.168.108.126', 'faek', 'faek')
-iosvl2.open()
+# ios_driver = get_network_driver('ios')
+# iosvl2 = ios_driver('192.168.108.126', 'faek', 'faek')
+# iosvl2.open()
+# ios_output = iosvl2.get_facts()
+# print(json.dumps(ios_output, indent=5))
+# iosvl2.close()
 
-ios_output = iosvl2.get_facts()
-print(ios_output)
+
+# driver = get_network_driver('eos')
+# eosl2 = driver('192.168.108.128', 'faek', 'faek')
+# eosl2.open()
+# eos_output = eosl2.get_facts()
+# print(json.dumps(eos_output, indent=4))
+# eosl2.close()
+
+ios_driver = get_network_driver('ios')
+iosvl2 = ios_driver('192.168.108.126', 'faek', 'faek')
+iosvl2.open()
+ios_output = iosvl2.get_interfaces()
+print(json.dumps(ios_output, indent=5))
+iosvl2.close()
+
+
+driver = get_network_driver('eos')
+eosl2 = driver('192.168.108.128', 'faek', 'faek')
+eosl2.open()
+eos_output = eosl2.get_interfaces()
+print(json.dumps(eos_output, indent=4))
+eosl2.close()
