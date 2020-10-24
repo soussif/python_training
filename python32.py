dd8211d84277066a67b1990f2cd53d139984a424
Faek Soussi <soussif@gmail.com>
2020-10-24 17:03:15 +0200

    first commit

---
+++ python32.py
@@ -0,0 +1,26 @@
+import getpass
+import telnetlib
+
+HOST = "192.168.108.126"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+
+tn.write(b"conf t\n")
+for i in range(2,12):
+    tn.write(b"vlan "+ str(i).encode('ascii')  + b"\n")
+    tn.write(b"name Python_VLAN_"+ str(i).encode('ascii') + b"\n")
+
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
