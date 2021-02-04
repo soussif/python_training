4413336101bc6419b46cbae734de6ab82b922e13
Faek Soussi <faek.soussi@imc.com>
2021-02-04 15:19:13 +0100

    modified json

--- python_youtube_videos/json_stuff.py
+++ python_youtube_videos/json_stuff.py
@@ -72,3 +72,25 @@ def decode_user(dct):
 user = json.loads(userJSON, object_hook=decode_user)
 print(type(user))
 print(user.name)
+
+
+def decode(data, default=None):
+    # Load JSON data from a string.
+    # Args:
+    # data: JSON data to decode.
+    # default: Value to return if decoding fails.
+    # Defaults to an empty dictionary.
+    if default is None:
+        default = {}
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+
+foo = decode('bad data')
+foo['stuff']= 5
+bar = decode('also bad')
+bar['meep'] = 1
+print("Foo: ", foo)
+print("Bar: ", bar)
