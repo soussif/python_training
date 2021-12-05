a83990c471e590b73e91e4d8716d85ac37ebd149
Faek Soussi <soussif@gmail.com>
2021-12-05 04:24:36 -0800

    updated testing

--- SQLAlchemy/src/test_unit.py
+++ SQLAlchemy/src/test_unit.py
@@ -15,7 +15,8 @@ def db_mock():
     """
     Test fixture setup to create sample database from "model" data.
     """
-    return database.Database("src/data/db.json")
+    # return database.Database("src/data/db.json")
+    return database.Database("sqllite:///:memory:", "src/data/initial.json")
 
 
 def test_balance(db_mock):
@@ -23,10 +24,12 @@ def test_balance(db_mock):
     Test the "balance()" method. This doesn't follow TDD since we
     wrote the code for this method a long time ago.
     """
+    db_mock.connect()
     assert db_mock.balance("ACCT100") == "40.00 USD"
     assert db_mock.balance("ACCT200") == "-10.00 USD"
     assert db_mock.balance("ACCT300") == "0.00 USD"
     assert db_mock.balance("nick123") is None
+    db_mock.disconnect()
 
 
 def test_owes_money(db_mock):
@@ -34,7 +37,9 @@ def test_owes_money(db_mock):
     Test the "owes_money()" method. This does follow TDD since we
     wrote this test before the method was implemented.
     """
+    db_mock.connect()
     assert db_mock.owes_money("ACCT100")
     assert not db_mock.owes_money("ACCT200")
     assert not db_mock.owes_money("ACCT300")
     assert db_mock.owes_money("nick123") is None
+    db_mock.disconnect()
